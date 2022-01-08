7e183521cc8d12f8d9e734a132b0e6d1295a9662
mogoatlhe <iam@ofent.se>
2022-01-08 15:14:01 +0200

    added functionality for basic math operations

---
+++ script/index.js
@@ -0,0 +1,6 @@
+
+// basic operations
+const add = (leftOperand, rightOperand) => leftOperand + rightOperand;
+const subtract = (leftOperand, rightOperand) => leftOperand - rightOperand;
+const multiply = (leftOperand, rightOperand) => leftOperand * rightOperand;
+const divide = (leftOperand, rightOperand) => leftOperand / rightOperand;
