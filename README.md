c21591f946eb6d549f933c5ea8c58a68e51046bf
mogoatlhe <iam@ofent.se>
2022-01-11 23:18:09 +0200

    implemented division by 0 error handling

--- README.md
+++ README.md
@@ -4,15 +4,15 @@
 Calculator functionality implemented in JavaScript as part of [The Odin Project](https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/calculator) curriculum.
 
 ## Requirements
-- Does not follow BODMAS, i.e `12 + 7 - 5 * 3` should add up to  `42` instead of `4`
-- The above statement means chained calculations are operated on `a pair at a time`
- + `12 + 7 - 5 * 3`
- + `19 - 5 * 3`
- + `14 * 3`
- + `42`
-- Avoid numbers from overflowing the screen, for me this only applies to answer. The chained operations can overflow because the design inspiration is from my phone's calculator and my phone's calculator has chained operations #overflowing.
-- Pressing "Clear" / "AC" should reset all the data allowing the user to start afresh.
-- Display a snarky message when division by 0 is attempted
+- [x]  Does not follow BODMAS, i.e `12 + 7 - 5 * 3` should add up to  `42` instead of `4`
+  - The above statement means chained calculations are operated on `a pair at a time`
+  + `12 + 7 - 5 * 3`
+  + `19 - 5 * 3`
+  + `14 * 3`
+  + `42`
+- [x] Avoid numbers from overflowing the screen, for me this only applies to answer. The chained operations can overflow because the design inspiration is from my phone's calculator and my phone's calculator has chained operations #overflowing.
+- [x] Pressing "Clear" / "AC" should reset all the data allowing the user to start afresh.
+- [x] Display a snarky message when division by 0 is attempted
 - Allow calculations on floating point numbers, and prevent users from entering numbers such as `12.3.56.5.`
  + Disable `.` if it already exists on current operand
 - Add a backspace button for the user to undo if they click the wrong number.
--- script/index.js
+++ script/index.js
@@ -1,14 +1,35 @@
 
-let answer;
 let prevLeft = "";
 let operator = "";
 let leftOperand = "";
 let rightOperand = "";
+let pressedValue = "";
+let pressedId = "";
 
+const clear = document.getElementById("ac");
+const answer = document.getElementById("answer");
+const screen = document.getElementById("values-display");
 const operands = document.getElementsByClassName("operand");
 const operators = document.getElementsByClassName("operator");
-const screen = document.getElementById("values-display");
+const dot = document.getElementById("dot");
 screen.textContent = "";
+
+resetData = (body, id) => {
+    let cursorColor = getComputedStyle(body).getPropertyValue("--cursor-color");
+
+    if(cursorColor !== "#a2c0f1" || id === "ac"){ // resetValues
+        body.style.setProperty("--cursor-color", "#a2c0f1");
+
+        if(!isNaN(pressedValue) || id === "ac"){
+            leftOperand = inputString = "";
+            screen.textContent = "";
+            answer.textContent = "";
+        }
+
+        rightOperand = "";
+        operator = "";
+    }
+}
 
 setValues = element => {
 
@@ -19,37 +40,48 @@ setValues = element => {
     element.addEventListener("click", () => {
 
         let tempInputStr = "";
-        let pressedValue = element.textContent;
-        let pressedId = element.getAttribute("id");
-        let cursorColor = getComputedStyle(body).getPropertyValue("--cursor-color");
         let screenColor = getComputedStyle(body).getPropertyValue("--screen-color");
+        
+        pressedValue = element.textContent;
+        pressedId = element.getAttribute("id");
 
         inputString = tempInputStr = screen.textContent;
 
-        if(cursorColor !== "#a2c0f1"){ // resetValues
-            body.style.setProperty("--cursor-color", "#a2c0f1");
-
-            if(!isNaN(pressedValue)){
-                leftOperand = "";
-                inputString = "";
-            }
-
-            rightOperand = "";
-            operator = "";
+        if(inputString.length > 8){ // blink
+            answer.classList.add("error");
+            answer.textContent = "too long"
+            return;
         }
+        
+        answer.classList.remove("error");
+        
+        resetData(body);
 
         if(!isNaN(pressedValue)){
             if(operator !== ""){
                 inputString = inputString.replace(new RegExp(`${rightOperand}$`, "g"), "");
                 
                 if(inputString === tempInputStr){
+
+                    if(pressedValue === "0" && operator === "÷"){
+                        answer.classList.add("error");
+                    }
+                    
+                    prevLeft = leftOperand;
                     rightOperand = pressedValue;
+                    answer.textContent = operate(operator, Number(leftOperand), Number(rightOperand));
                 }else{
                     rightOperand += pressedValue;
+                    answer.textContent = operate(operator, Number(prevLeft), Number(rightOperand));
+                }
+                
+                
+                // if divide by 0, a string is returned
+                // avoid assigning a NaN to leftOperand
+                if(!isNaN(answer.textContent)){
+                    leftOperand = answer.textContent;
                 }
 
-                prevLeft = leftOperand;
-                leftOperand = answer = operate(operator, Number(leftOperand), Number(rightOperand));
                 inputString += rightOperand;
             }else{
                 inputString = inputString.replace(new RegExp(`${leftOperand}$`, "g"), "");
@@ -79,16 +111,36 @@ setValues = element => {
             
             if(isNaN(previousPressed)){
                 inputString = inputString.replace(operator, "");
+            }else if(previousPressed === "0" && operator === "÷"){
+                inputString = inputString.replace(/(÷0)$/, "");
+                answer.textContent = "";
             }
 
             operator = pressedValue;
             inputString += operator;
         }else if(operator !== "" && pressedId === "equals"){
-            answer = operate(operator, Number(prevLeft), Number(rightOperand));
-            inputString = leftOperand = answer;
+            answer.textContent = operate(operator, Number(prevLeft), Number(rightOperand));
+
+            if(isNaN(answer.textContent)){
+                answer.classList.add("error");
+                return;
+            }
+
+            inputString = leftOperand = answer.textContent;
             body.style.setProperty("--cursor-color", screenColor);
         }
         screen.textContent = inputString;
+    });
+    
+    clear.addEventListener("click", () => {
+        
+        resetData(body, clear.getAttribute("id"));
+
+        // if(inputString.length > 0){
+        //     let lastCharacter = inputString[inputString.length - 1];
+        //     inputString = inputString.replace(new RegExp(`(${lastCharacter})$`, "g"), "");
+        //     screen.textContent = inputString;
+        // }
     });
 }
 
@@ -103,24 +155,29 @@ const divide = (leftOperand, rightOperand) => leftOperand / rightOperand;
 
 const operate = (operator, leftOperand, rightOperand) => {
 
+    let answer = null;
     const error = `Invalid operator: ${operator}`;
 
     if(operator === "+"){
-        return add(leftOperand, rightOperand);
-    }
-
-    if(operator === "-"){
-        return subtract(leftOperand, rightOperand);
+        answer = add(leftOperand, rightOperand);
+    }else if(operator === "-"){
+        answer = subtract(leftOperand, rightOperand);
+    }else if(operator === "x"){
+        answer = multiply(leftOperand, rightOperand);
+    }else if(operator === "÷"){
+        if(rightOperand === 0){
+            answer =  "Can't divide by 0";
+        }else{
+            answer = divide(leftOperand, rightOperand);
+        }
+    }else{
+        answer = error;
     }
     
-    if(operator === "x"){
-        return multiply(leftOperand, rightOperand);
+    if(!isNaN(answer) && !Number.isInteger(answer)){
+        answer = Number.parseFloat(answer).toFixed(2);
     }
 
-    if(operator === "÷"){
-        return divide(leftOperand, rightOperand);
-    }
-
-    return error;
+    return answer;
 
 }
